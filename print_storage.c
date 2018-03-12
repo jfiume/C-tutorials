7b8e644f9c6a253db7510302ce0dffc47d7c14bf
Joe Fiume <joefiume88@gmail.com>
2018-03-12 14:06:33 -0700

    updated print storage

--- print_storage.c
+++ print_storage.c
@@ -2,6 +2,10 @@
 #include <limits.h>
 
 int main() {
+  printf("Storage size for float : %d \n", sizeof(float));
+  // printf("Maximum float positive value : %E \n", FLT_MIN );
+  // printf("Maximum float positive value : %E \n", FLT_MAX );
+  // printf("Precision value: %d\n", FLT_DIG );
   printf("Storage size for int : %d \n", sizeof(int));
 
   return 0;
