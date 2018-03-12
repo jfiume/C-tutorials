6dea9ce257580d335e7c9c49a5f7fb8199f29793
Joe Fiume <joefiume88@gmail.com>
2018-03-12 13:39:23 -0700

    Initial Commit

---
+++ C-tutorials/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-tutorials
+//
+//  Created by Joe on 3/12/18.
+//  Copyright © 2018 Joe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
